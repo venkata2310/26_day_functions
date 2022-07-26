b4d517878a9f0e9808b9b476431977d936c28f17
VENKATA RAGURAM KN <107139560+venkata2310@users.noreply.github.com>
2022-07-26 21:40:11 +0530

    Add files via upload

---
+++ function_1.py
@@ -0,0 +1,21 @@
+# def nineam() :   #Function Declaration
+#     print('How is your day?')
+# nineam()   # Function call
+
+# def hello(n) :
+#     print('we are going to start '+n)
+# hello('python')
+
+# def hi(fname,lname):
+#     print('my name is ',fname, 'my surname is ',lname)
+# hi('nvr','k')
+
+# def world(z):
+#     print('Welcome to the war of 1914 '+z)
+# world('world war 1')
+# world('ww2')
+
+# def mobile(*m):
+#     print('Mobile Brand',m)
+# mobile('Redmi','Realme','Poco','Vivo','Oppo')
+# mobile(['Asus','Moto','IQ','Nothing'])
